02b50bad38af5de27258b26f7b4676a9533926d0
Itssxc <itssxc999@gmail.com>
2025-06-23 23:48:40 -0700

    fix: make Flask public for Render

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    app.run(debug=True, host='0.0.0.0', port=10000) 
